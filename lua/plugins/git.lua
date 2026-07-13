e01f4b33a13e0f0f6bb7953ce774c373118c6041
daibeemachine <daibeemachine@gmail.com>
2026-07-13 13:51:54 -0500

    Added neogit file (I forgot to add it before I guess)

---
+++ lua/plugins/git.lua
@@ -0,0 +1,10 @@
+return
+{
+    "NeogitOrg/neogit",
+    dependencies = {
+    "sindrets/diffview.nvim",        -- optional
+    "esmuellert/codediff.nvim",      -- optional
+    "m00qek/baleia.nvim",            -- optional
+    }
+}
+
